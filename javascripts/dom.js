44956579b674181588fd55113b688445ef5d21f3
Andy <amillion3@gmail.com>
2018-04-14 20:42:23 -0500

    styling updated

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -9,17 +9,19 @@ const buildDomString = petData => {
   let output = '';
   for (let i = 0; i < petData.length; i++) {
     output += `
-    <div class="panel panel-default text-center ${petData[i].typeOfPet}">
-    <div class="panel-heading">
-      <h3 class="panel-title">${petData[i].name}</h3>
+    <div class="col-sm-4">
+      <div class="panel panel-default text-center ${petData[i].typeOfPet}">
+        <div class="panel-heading">
+          <h3 class="panel-title">${petData[i].name}</h3>
+        </div>
+        <div class="panel-body">
+          <img src="${petData[i].imageURL}">
+          <h5>${petData[i].color}</h5>
+          <h5>${petData[i].specialSkill}</h5>
+        </div>
+        <div class="panel-footer type-${petData[i].typeOfPet}">${petData[i].typeOfPet}</div>
+      </div>
     </div>
-    <div class="panel-body">
-      <img src="${petData[i].imageURL}">
-      <h5>${petData[i].color}</h5>
-      <h5>${petData[i].specialSkill}</h5>
-    </div>
-    <div class="panel-footer type-${petData[i].typeOfPet}">${petData[i].typeOfPet}</div>
-  </div>
     `;
   }
   printToDom(output);
